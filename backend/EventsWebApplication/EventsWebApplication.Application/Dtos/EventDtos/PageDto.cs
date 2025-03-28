a67c8151bc1d3e2bfe8cee47b861311b94b10432
Kirill <kavrusevich2002@gmail.com>
2025-03-28 13:56:39 +0300

    feat: add auto-validation for paginaton

---
+++ backend/EventsWebApplication/EventsWebApplication.Application/Dtos/EventDtos/PageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EventsWebApplication.Application.Dtos.EventDtos
+{
+    public record PageDto
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ backend/EventsWebApplication/EventsWebApplication.Application/Validators/Events/PageValidator.cs
@@ -0,0 +1,17 @@
+﻿using EventsWebApplication.Application.Dtos.EventDtos;
+using FluentValidation;
+
+namespace EventsWebApplication.Application.Validators.Events
+{
+    public class PageValidator : AbstractValidator<PageDto>
+    {
+        public PageValidator()
+        {
+            RuleFor(e => e.Page)
+                .Must(page => page > 0).WithMessage("Page number can't be less than 1");
+
+            RuleFor(e => e.PageSize)
+                .Must(pageSize => pageSize > 0).WithMessage("Page size can't be less than 1");
+        }
+    }
+}
